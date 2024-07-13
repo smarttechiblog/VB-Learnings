9f9397039b62ceb276d62c541bd4cc0f9185d240
smarttechi <smarttechiblog@gmail.com>
2024-07-13 12:02:50 +0530

    added new program for custom sort string using strin.compare

--- CSharpCodingInterviewQuestions/Program.cs
+++ CSharpCodingInterviewQuestions/Program.cs
@@ -2,11 +2,15 @@
 
 // See https://aka.ms/new-console-template for more information
 using CSharpCodingInterviewQuestions.CustomSortingNumbers;
+using CSharpCodingInterviewQuestions.CustomSortingStrings;
 
 Console.WriteLine("Welcome to VBlearning...");
 
-CustomSort.listValues = new List<int> { 4, 1, 2, 3 };
-CustomSort.CallSort();
+//CustomSort.listValues = new List<int> { 4, 1, 2, 3 };
+//CustomSort.CallSort();
+CustomSortString.valuesToSort = new List<string> { "abc", "aa", "bbadd", "test" };
+CustomSortString.CallStringSort();
+
 
 Console.ReadKey();
 
